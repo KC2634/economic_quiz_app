ce3e8789dc2e9aec279b54b120987cb5dde533d2
Keeley <clough.kee@nulondon.ac.uk>
2026-02-26 13:05:27 +0000

    adds files from upload

---
+++ main copy.py
@@ -0,0 +1,240 @@
+import tkinter as tk # for creating the GUI
+from tkinter import messagebox
+import csv # to write to csv from main
+from quiz_data import load_questions # for loading the questions
+# utility functions to clean and validate names 👇
+from quiz_utils import clean_name, character_check, length_check, presence_check, pattern_check
+from datetime import datetime # to record a timestamp
+
+
+BG = "#D70032"
+BUTTON_BG = "#F2F2F2"
+TEXT = "#FFFFFF"
+BUTTON_TEXT = "#111111"
+
+questions = load_questions()
+
+class EconomicQuizApp(tk.Tk):
+    
+    """
+    A class that represents the quiz application.
+    """
+    def onFrameConfigure(canvas):
+        canvas.configure(scrollregion=canvas.bbox("all"))
+    def __init__(self, questions):
+        super().__init__()
+        self.title("Economic Quiz")
+        self.questions = questions
+        self.current_question = 0
+        self.score = 0
+        self.configure(bg=BG)
+        self.geometry("700x2000")
+        self.name = tk.StringVar()
+        self.answer_var = tk.IntVar(value=-1)
+        self.answer_vars = []
+
+    
+
+    
+        self.name_label = tk.Label(
+            self,
+            text="Please enter your name in the box below 😊",
+            bg=BG,
+            fg=TEXT,
+            font=("Arial", 18, "bold")
+        )
+        self.name_label.pack(pady=15)
+
+        self.name_entry = tk.Entry(
+            self,
+            textvariable=self.name,
+            font=("Helvetica", 18),
+            fg=BUTTON_TEXT
+        )
+        self.name_entry.pack(pady=15)
+
+        self.build_question_screen()
+
+        self.submit_button = tk.Button(
+            self,
+            text="SUBMIT!",
+            font=("Arial", 18),
+            fg=BUTTON_TEXT,
+            bg=BUTTON_BG,
+            command=self.handle_submit
+        )
+        self.submit_button.pack(pady=15)
+
+    def build_question_screen(self):
+        
+        """
+        Builds the question section.
+        """
+
+        question_number = 1
+
+        for question in self.questions:
+            q_label = tk.Label(
+                self,
+                text=f"Question {question_number}. {question['question']}",
+                font=("Helvetica", 18),
+                wraplength=700,  # wrap the text if it's too long
+                justify="center",
+                fg = TEXT,  
+                bg = BG
+            )
+            q_label.pack(anchor="w", padx=40, pady=(20, 5))
+
+            answer_var = tk.IntVar(value=-1)
+            self.answer_vars.append(answer_var)
+
+            option_value = 0
+            for option in question["options"]:
+                rb = tk.Radiobutton(
+                    self,
+                    text=option,
+                    variable=answer_var,
+                    value=option_value,
+                    font=("Helvetica", 14),
+                    fg = TEXT,
+                    bg=BG
+                )
+                rb.pack(anchor="w", padx=60)
+                option_value += 1
+
+            question_number += 1
+    
+    def handle_submit(self):
+        
+        skipped_questions = [i + 1 for i, var in enumerate(self.answer_vars) if var.get() == -1]
+        if skipped_questions:
+            missing_str = ", ".join(map(str, skipped_questions))
+            messagebox.showwarning(
+                "Incomplete Quiz", 
+                f"Please answer all questions before submitting. Missing questions: {missing_str}"
+            )
+            return
+
+
+        st_name = clean_name(self.name_entry.get())
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        if self.validate_name_with_messages(st_name):
+            self.score = 0
+            for i, var in enumerate(self.answer_vars):
+                user_choice = var.get()
+                correct_choice = self.questions[i]["correct"]
+
+                if user_choice == correct_choice:
+                    self.score += 1
+
+            answers = []
+            for var in self.answer_vars:
+                answers.append(var.get())
+                    
+            with open("quiz_records.csv", mode="a", newline="", encoding="utf-8") as file:
+                writer = csv.writer(file)
+                writer.writerow([st_name, timestamp, self.score, answers])
+                print("Data successfully written to CSV.")
+
+            self.build_thank_you_screen(st_name)
+            
+            
+    def build_thank_you_screen(self, name):
+        
+        """Shows the final confirmation screen."""
+
+        self.clear_screen()
+
+        tk.Label(
+            self,
+            text=f"Thank you for submitting your answers, {name}!",
+            font=("Helvetica", 18),
+            fg = TEXT,
+            bg = BG
+        ).pack(pady=20)
+
+        tk.Label(
+            self,
+            text=f"{name}, your score is:{self.score}/{len(self.questions)}",
+            font=("Helvetica", 24),
+            wraplength=500,
+            justify="center",
+            fg = TEXT,
+            bg = BG
+        ).pack(pady=10)
+
+        tk.Label(
+            self,
+            text="Well done, keep up the hard work!",
+            font=("Helvetica", 18),
+            wraplength=500,
+            justify="center",
+            fg = TEXT,
+            bg = BG
+        ).pack(pady=20)
+
+        tk.Button(
+            self,
+            font=("Helvetica", 18),
+            fg=BUTTON_TEXT,
+            bg="#F2F2F2",
+            text="QUIT",
+            command=self.destroy
+        ).pack(pady=30)
+
+        self.logo_image = tk.PhotoImage(file="Logo.png")
+        self.logo_image = self.logo_image.zoom(2,2)
+        self.logo_image = self.logo_image.subsample(2,2)
+        self.logo_label = tk.Label(self, image=self.logo_image, bg=BG)
+        self.logo_label.pack(pady=20)
+
+        
+    def validate_name_with_messages(self, cleaned_name: str) -> bool:
+        
+        """
+        Validates the cleaned name and shows an error message if invalid.
+        Returns True if the name is valid. Not pure because depends on messagebox.
+        """
+        valid = True
+
+        if not presence_check(cleaned_name):
+            messagebox.showerror(
+                "Invalid name",
+                "Please enter your name."
+            )
+            valid = False
+
+        if valid and not length_check(cleaned_name):
+            messagebox.showerror(
+                "Invalid name",
+                "Name must be between 2 and 50 characters."
+            )
+            valid = False
+
+        if valid and not character_check(cleaned_name):
+            messagebox.showerror(
+                "Invalid name",
+                "Names must only contain letters, spaces, or hyphens."
+            )
+            valid = False
+
+        if valid and not pattern_check(cleaned_name):
+            messagebox.showerror(
+                "Invalid name",
+                "Names must only contain letter, spaces or hyphens."
+            )
+            valid = False
+
+        return valid
+
+    def clear_screen(self):
+        
+        """Removes all widgets from the window."""
+        
+        for widget in self.winfo_children():
+            widget.destroy()
+
+if __name__ == "__main__":
+    app = EconomicQuizApp(questions)
+    app.mainloop()
---
+++ test_smoke_and_all copy.py
@@ -0,0 +1,53 @@
+import unittest # a testing framework
+from quiz_data import load_questions # converts to dict
+from quiz_utils import clean_name, presence_check, character_check, pattern_check, length_check
+
+
+class TestSmoke(unittest.TestCase):
+
+    def test_ut_works(self):
+        self.assertEqual(2+2,4)
+        self.assertNotEqual(6,1)
+
+    def test_load_questions_runs(self):
+        self.assertTrue(1)
+
+class TestQuiz(unittest.TestCase):
+
+    def test_load_questions_runs(self):
+        questions = load_questions()
+        self.assertIsNotNone(questions)
+
+    def test_presence_check_happy(self):
+        self.assertTrue(presence_check("Jo"))
+        
+    def test_clean_name(self):
+        self.assertEqual(clean_name("  josh done  "), "Josh Done")
+        self.assertEqual(clean_name("BON JONES"), "Bon Jones")
+        
+    def test_character_check_happy(self):
+        self.assertTrue(character_check("Jo"))
+        self.assertTrue(character_check("Joseph Smith"))
+
+    def test_character_check_unhappy(self):
+        self.assertFalse(character_check("Jess1"))
+        self.assertFalse(character_check("897"))
+
+    def test_symbol_check_happy(self):
+        self.assertTrue(pattern_check("Anna-Marie"))
+        self.assertTrue(pattern_check("Jo-C"))
+
+    def test_symbol_check_unhappy(self):
+        self.assertFalse(pattern_check("Anna]k"))
+        self.assertFalse(pattern_check("Jo/C"))
+
+    def test_length_check_happy(self):
+        self.assertTrue(length_check("Jo"))
+        self.assertTrue(length_check("Hanahhhhhhhhhhhhhhhhhhhhhhhhhhhh"))
+
+    def test_length_check_unhappy(self):
+        self.assertFalse(length_check("J"))
+        self.assertFalse(length_check("Hanahhhhhhhhhhhhhhhhhhhhhhhhhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+
+if __name__ == "__main__":
+    unittest.main()
